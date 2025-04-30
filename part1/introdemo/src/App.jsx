c6c9e0d139f03210861b4c7ed3443aaa416d3a92
cristinabaldor <ccbaldor@icloud.com>
2025-04-30 16:53:23 -0400

    React reading 1

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,7 +1,19 @@
-const App = () => {
+const Hello = () => {
   return (
     <div>
       <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 }
